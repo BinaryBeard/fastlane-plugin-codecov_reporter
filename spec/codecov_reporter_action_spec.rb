f7385a19f00fc53e0d08f50f0f01fe66e315094c
BinaryBeard <sam.bantner@me.com>
2017-08-09 17:02:52 -0400

    New test

--- spec/codecov_reporter_action_spec.rb
+++ spec/codecov_reporter_action_spec.rb
@@ -1,8 +1,13 @@
 describe Fastlane::Actions::CodecovReporterAction do
   describe '#run' do
-    it 'prints a message' do
+    it 'prints all the messages' do
+      expect(Fastlane::UI).to receive(:message).with("I am Getting the latest bash script from Codecov.io")
+      expect(Fastlane::UI).to receive(:message).with("It looks like I'm working with a public repository")
+      expect(Fastlane::UI).to receive(:message).with("Removing the bash script I got from Codecov.io")
+      expect(Fastlane::UI).to receive(:message).with("Removing the created coverage.txt files")
+      expect(Fastlane::UI).to receive(:message).with("All was well")
 
-      Fastlane::Actions::CodecovReporterAction.run(nil)
+      Fastlane::Actions::CodecovReporterAction.run({})
     end
   end
 end
