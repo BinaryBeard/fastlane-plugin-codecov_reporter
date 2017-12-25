b998cac54833633699def99a29b6777c7eae5a8e
Fabio Milano <fabio@loot.io>
2017-12-26 00:45:36 +0100

    fix tests

--- spec/codecov_reporter_action_spec.rb
+++ spec/codecov_reporter_action_spec.rb
@@ -4,7 +4,7 @@ describe Fastlane::Actions::CodecovReporterAction do
       expect(Fastlane::UI).to receive(:message).with("I am Getting the latest bash script from Codecov.io")
       expect(Fastlane::UI).to receive(:message).with("It looks like I'm working with a public repository")
       expect(Fastlane::UI).to receive(:message).with("Removing the bash script I got from Codecov.io")
-      expect(Fastlane::UI).to receive(:message).with("Removing the created coverage.txt files")
+      expect(Fastlane::UI).to receive(:message).with("Removing the created coverage.txt files, if any.")
       expect(Fastlane::UI).to receive(:message).with("All was well")
 
       Fastlane::Actions::CodecovReporterAction.run({})
